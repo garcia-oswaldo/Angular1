e46e80fdd815949a174dd9efbaa0e8a227b7b96c
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-12-20 13:33:46 -0600

    first angular set up and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular1';
+  // title = 'my-dream-app';
+  name='Ozzy';
 }
